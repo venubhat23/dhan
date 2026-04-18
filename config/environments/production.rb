632b2090afb127043d3c994f823816b0408d045d
venu bhat <venubha147@gmail.com>
2026-04-18 16:58:57 +0530

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on Cloudflare R2 (see config/storage.yml for options).
-  config.active_storage.service = :cloudflare_r2
+  # Store uploaded files locally in production (temporarily until credentials are set)
+  config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
